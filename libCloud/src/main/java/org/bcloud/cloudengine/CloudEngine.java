768b21b1702d00adf05866928790c4b6db535e15
Blu <110566084+Bluismine@users.noreply.github.com>
2023-10-11 12:27:41 +0700

    init libCloud

---
+++ libCloud/src/main/java/org/bcloud/cloudengine/CloudEngine.java
@@ -0,0 +1,12 @@
+package org.bcloud.cloudengine;
+
+public class CloudEngine {
+
+    private static void runTest() {
+
+    }
+
+    public static void main(String[] args) {
+        runTest();
+    }
+}
