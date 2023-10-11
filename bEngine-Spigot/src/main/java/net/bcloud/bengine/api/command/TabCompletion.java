ca3d611054adf2346018aa361e1c380d7ee63104
Bluismine <110566084+bluismine@users.noreply.github.com>
2023-10-11 12:29:00 +0700

    Change name of bEngine to libCloud
    Delete bEngine-Spigot directory

--- bEngine-Spigot/src/main/java/net/bcloud/bengine/api/command/TabCompletion.java
+++
@@ -1,6 +0,0 @@
-package net.bcloud.bengine.api.command;
-
-import org.bukkit.command.TabCompleter;
-
-public interface TabCompletion extends TabCompleter {
-}
--- bEngine-Spigot/src/main/java/net/bcloud/bengine/api/inventory/item/ItemClick.java
+++
@@ -1,8 +0,0 @@
-package net.bcloud.bengine.api.inventory.item;
-
-import net.bcloud.bengine.api.inventory.InventoryViewer;
-import org.bukkit.event.inventory.InventoryClickEvent;
-
-public interface ItemClick {
-    void click(InventoryViewer viewer, InventoryClickEvent event);
-}
